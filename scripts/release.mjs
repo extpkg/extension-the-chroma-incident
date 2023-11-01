bfb6df839b11b934d57806dacb8e0931ad24c1ff
Bodgan B Peterson <github@bogdanbpeterson.com>
2023-11-01 16:56:04 +0200

    chore: miscellaneous update

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 
+import { execSync } from "node:child_process";
+import { existsSync } from "node:fs";
 import * as esbuild from "esbuild";
 import {
   clean,
@@ -22,3 +24,17 @@ export const build = async () => {
 };
 
 build();
+
+if (!existsSync("key.pem")) {
+  console.error("key.pem file not found!");
+  process.exit(1);
+}
+
+try {
+  execSync("npx ext-packager pack ./dist -k key.pem -o extension.ext -f", {
+    stdio: "inherit",
+  });
+} catch (error) {
+  console.error("Error running npm run release:", error);
+  process.exit(1);
+}
